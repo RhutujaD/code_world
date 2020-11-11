b4da0cb76117b2eabbff7445552e4be48941fea0
RhutujaD <42312594+RhutujaD@users.noreply.github.com>
2020-11-11 08:00:41 +0530

    Add files via upload

---
+++ dotNET/Day18_Ass/Task1/Task1/dbo.Employees.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Employees]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [name] NVARCHAR(MAX) NOT NULL, 
+    [salary] FLOAT NOT NULL
+)
---
+++ dotNET/Day18_Ass/Task1/Task1/dbo.Table.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(MAX) NULL, 
+    [Salalry] FLOAT NULL
+)
